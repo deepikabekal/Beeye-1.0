76a6a9b8a6525e11071f94627f9adab003d41581
deepikabekal <78413654+deepikabekal@users.noreply.github.com>
2021-10-01 17:07:04 +0400

    initial commit

--- src/components/TeamSection/index.js
+++ src/components/TeamSection/index.js
@@ -1,8 +1,14 @@
 import React from "react";
+import GraphSection from "./GraphSection";
 import ResourcePlanning from "./ResourcePlanning";
 
 function TeamSection() {
-  return <ResourcePlanning />;
+  return (
+    <>
+      <GraphSection />
+      <ResourcePlanning />
+    </>
+  );
 }
 
 export default TeamSection;
