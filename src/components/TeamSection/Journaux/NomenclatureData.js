adb98d34f400d754a2257cbb6759c263aae29b5b
deepikabekal <78413654+deepikabekal@users.noreply.github.com>
2021-10-02 19:28:53 +0400

    added new value to nomenclature data

--- src/components/TeamSection/Journaux/NomenclatureData.js
+++ src/components/TeamSection/Journaux/NomenclatureData.js
@@ -9,14 +9,24 @@ import {
   faThList
 } from "@fortawesome/free-solid-svg-icons";
 const NomenclatureData = [
-  { id: 1, name: "Description", icon: faFileAlt },
-  { id: 2, name: "Statut", icon: faCircle },
-  { id: 3, name: "Équipe", icon: faUsers },
-  { id: 4, name: "Honoraires", icon: faEnvelopeOpenText },
-  { id: 5, name: "Durée", icon: faStopwatch },
-  { id: 6, name: "Dates", icon: faCalendarAlt },
-  { id: 7, name: "Checklist", icon: faTasks },
-  { id: 8, name: "Champs", icon: faThList }
+  {
+    id: 1,
+    name: "Description",
+    icon: faFileAlt,
+    displayValue: null
+  },
+  { id: 2, name: "Statut", icon: faCircle, displayValue: null },
+  { id: 3, name: "Équipe", icon: faUsers, displayValue: "Gabrielle Chevalier" },
+  {
+    id: 4,
+    name: "Honoraires",
+    icon: faEnvelopeOpenText,
+    displayValue: "100 € / h"
+  },
+  { id: 5, name: "Durée", icon: faStopwatch, displayValue: "2 h" },
+  { id: 6, name: "Dates", icon: faCalendarAlt, displayValue: "11 / 11 / 2020" },
+  { id: 7, name: "Checklist", icon: faTasks, displayValue: "1 / 5" },
+  { id: 8, name: "Champs", icon: faThList, displayValue: null }
 ];
 
 export default NomenclatureData;
