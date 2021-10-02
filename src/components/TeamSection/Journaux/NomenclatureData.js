a8b70e9ec6d1806e24a12fccbe894db1011c19bb
deepikabekal <78413654+deepikabekal@users.noreply.github.com>
2021-10-02 18:27:19 +0400

    data for nomenclature bar buttons added

---
+++ src/components/TeamSection/Journaux/NomenclatureData.js
@@ -0,0 +1,22 @@
+import {
+  faFileAlt,
+  faCircle,
+  faUsers,
+  faEnvelopeOpenText,
+  faStopwatch,
+  faCalendarAlt,
+  faTasks,
+  faThList
+} from "@fortawesome/free-solid-svg-icons";
+const NomenclatureData = [
+  { id: 1, name: "Description", icon: faFileAlt },
+  { id: 2, name: "Statut", icon: faCircle },
+  { id: 3, name: "Équipe", icon: faUsers },
+  { id: 4, name: "Honoraires", icon: faEnvelopeOpenText },
+  { id: 5, name: "Durée", icon: faStopwatch },
+  { id: 6, name: "Dates", icon: faCalendarAlt },
+  { id: 7, name: "Checklist", icon: faTasks },
+  { id: 8, name: "Champs", icon: faThList }
+];
+
+export default NomenclatureData;
