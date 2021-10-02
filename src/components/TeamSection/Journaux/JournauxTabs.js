8cae45c1f3538f09bc59616d4e24b737d7903d6c
deepikabekal <78413654+deepikabekal@users.noreply.github.com>
2021-10-02 18:41:32 +0400

    logic to render the nomenclature bar buttons dynamically

--- src/components/TeamSection/Journaux/JournauxTabs.js
+++ src/components/TeamSection/Journaux/JournauxTabs.js
@@ -1,2 +1,21 @@
 import React from "react";
 import Button from "../Button";
+import NomenclatureData from "./NomenclatureData";
+
+function JournauxTabs() {
+  return (
+    <div>
+      {NomenclatureData.map((item) => (
+        <Button
+          key={item.id}
+          btnClass=""
+          icon={item.icon}
+          iconClass=""
+          name={item.name}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default JournauxTabs;
--- src/components/TeamSection/Journaux/index.js
+++ src/components/TeamSection/Journaux/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Button from "../Button";
+import JournauxTab from "./JournauxTabs";
 import "./JournauxStyles.css";
 import { faSync, faPen } from "@fortawesome/free-solid-svg-icons";
 function Journaux() {
   return (
-    <div className="journaux-header div-border">
-      <Button btnClass="icon-btn" icon={faSync} iconClass="sync-icon-style" />
-      <h2 className="section-heading">Tenue des journaux</h2>
-      <Button btnClass="icon-btn" icon={faPen} iconClass="pen-icon-style" />
+    <div>
+      <div className="journaux-header div-border">
+        <Button btnClass="icon-btn" icon={faSync} iconClass="sync-icon-style" />
+        <h2 className="section-heading">Tenue des journaux</h2>
+        <Button btnClass="icon-btn" icon={faPen} iconClass="pen-icon-style" />
+      </div>
+      <JournauxTab />
     </div>
   );
 }
