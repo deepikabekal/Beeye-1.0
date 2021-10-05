443583e4983777fc2bceef0d21ae2a9e07f11173
deepikabekal <78413654+deepikabekal@users.noreply.github.com>
2021-10-05 09:52:29 +0400

    fixed TempPage render

--- src/components/TempPage/index.js
+++ src/components/TempPage/index.js
@@ -3,14 +3,22 @@ import "./TempPageStyle.css";
 
 function TempPage(props) {
   return (
-    <div>
-      <div className="temp-page-header pad-left-15">
-        <h2 className="temp-page-heading">{props.name}</h2>
-      </div>
-      <div>
-        <p className="user-msg pad-left-15">Page under development </p>
-      </div>
-    </div>
+    <>
+      {props.calledFrom === "leftmenu" ? (
+        <div>
+          <div className="temp-page-header pad-left-15">
+            <h2 className="temp-page-heading">{props.name}</h2>
+          </div>
+          <div>
+            <p className="user-msg pad-left-15">Page under development </p>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p className="user-msg pad-left-15">Page under development </p>
+        </div>
+      )}
+    </>
   );
 }
 
