0d3aae8d056feb1a0f244144a731af8b794c7476
deepikabekal <78413654+deepikabekal@users.noreply.github.com>
2021-10-06 01:09:46 +0400

    initial commit

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,33 +1,37 @@
 // This component renders the Task component
 
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import "./TaskStyles.css";
 import TaskHeader from "./TaskHeader";
 import TaskCard from "./TaskCard";
 
 function Task(props) {
   let [count, setCount] = useState(0); // for + click
-  let [isChecked, setChecked] = useState(false); //for select all checkbox click
+  const [isChecked, setIsChecked] = useState(false); //for select all checkbox click
 
   // This function will be called when the + button is clicked
-  // for every click, count will be incremented by 1 and the state change will recorded
-  // Later count value will be changed to Array.
-  //And using map function on count, TaskCard component will be render to the Task section
+  // for every click, count will be incremented by 1 and the state change will be recorded
+
   function addCard() {
     setCount(count + 1);
   }
 
+  // This function is called when the onChange event is triggered (select all checkbox is checked)
+  //if the select all check box is checked (true) then state is changed
+  // selectAll function is called else the state is changed and deSelectAll function is called
   function checkboxClick(event) {
     if (event.target.checked) {
-      setChecked(true);
+      setIsChecked(true);
       selectAll();
     } else {
-      setChecked(false);
+      setIsChecked(false);
       deSelectAll();
     }
   }
 
-  // This function will be called when the select all checkbox is clicked
+  // This function will be called when the select all checkbox is checked
+  //Using for loop all the other checkboxes in the Task section will be checked
+  //The count variable has the number of task
   function selectAll() {
     const box = document.getElementsByClassName("indiv-check-box");
     for (var i = 0; i < count; i++) {
@@ -35,10 +39,13 @@ function Task(props) {
     }
   }
 
+  // this function is called when the select all checkbox is unchecked
+  //Using for loop all the other checkboxes in the Task section will be unchecked
+  //The count variable has the number of task
   function deSelectAll() {
     const box = document.getElementsByClassName("indiv-check-box");
     for (var i = 0; i < count; i++) {
-      box[i].checked = true;
+      box[i].checked = false;
     }
   }
 
@@ -54,6 +61,8 @@ function Task(props) {
         />
         <label htmlFor="select-all">Sélectionner tout</label>
       </div>
+      {/* // count is changed to Array.
+  //Using map function on count, TaskCard component will be render to the Task section */}
       <div className="all-tasks-container">
         {[...Array(count)].map((_, index) => (
           <TaskCard key={index} />
